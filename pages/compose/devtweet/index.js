f95928ca34c702266e1b58620494f77d16e92368
lauradiaz <lauradiaz1586@gmail.com>
2021-05-11 13:15:54 +0400

    add constants por COMPOSE_STATES and route when creating a new devtweet

--- pages/compose/devtweet/index.js
+++ pages/compose/devtweet/index.js
@@ -1,14 +1,19 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
 import styles from "./devtweet.module.scss";
+
 import AppLayout from "src/components/AppLayout";
 import AppButton from "src/components/AppButton";
 import useUser from "src/hooks/useUser.js/index.js";
-import { useState } from "react";
+
 import { addDevtweet } from "src/firebase/client";
-
-
+import { COMPOSE_STATES } from "src/utils/constants";
 
 const DevTweet = () => {
   const user = useUser();
+  const router = useRouter();
+  const [status, setSatus] = useState(COMPOSE_STATES.USER_NOT_KNOWN);
   const [message, setMessage] = useState("");
 
   const handleChange = (event) => {
@@ -18,13 +23,22 @@ const DevTweet = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setSatus(COMPOSE_STATES.LOADING);
     addDevtweet({
       avatar: user.avatar,
       content: message,
       userId: user.uid,
       userName: user.username,
-    });
+    })
+      .then(() => {
+        router.push("/home");
+      })
+      .catch((err) => {
+        setSatus(COMPOSE_STATES.ERROR);
+      });
   };
+
+  const isButtonDisabled = !message.length && status === COMPOSE_STATES.LOADING;
 
   return (
     <AppLayout>
@@ -36,7 +50,7 @@ const DevTweet = () => {
           value={message}
         ></textarea>
         <div className={styles.div}>
-          <AppButton disabled={message.length === 0}>DevTweet</AppButton>
+          <AppButton disabled={isButtonDisabled}>DevTweet</AppButton>
         </div>
       </form>
     </AppLayout>
