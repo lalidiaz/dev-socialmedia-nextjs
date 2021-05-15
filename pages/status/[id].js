c840f24cb15f964db67066df4ea08f88793d6a96
lauradiaz <lauradiaz1586@gmail.com>
2021-05-15 23:22:51 +0400

    add url fixes in dynamic page

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -13,7 +13,9 @@ export async function getServerSideProps(context) {
   const { params, res } = context;
   const { id } = params;
 
-  const apiResponse = await fetch(`https://devtweeter.vercel.app/status/${id}`);
+  const apiResponse = await fetch(
+    `https://devtweeter.vercel.app/status//${id}`
+  );
   if (apiResponse.ok) {
     const props = await apiResponse.json();
     return { props };
